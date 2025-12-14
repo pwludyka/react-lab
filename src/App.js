0630677a07418235f0d52ccc408748451fb05df2
Pawel Wludyka <pwludyka@op.pl>
2025-12-14 11:46:04 +0100

    drugi

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import './App.css';
+import {useState} from "react";
 
 function App() {
-  let title = 'Shrek';
-
+  const [title, setTitle] = useState('');
+  
   function handleChange(event) {
-    console.log(event.target.value);
+    setTitle(event.target.value);
+  }  
+  
+  let message = "";
+
+  if (title.length < 5) {
+    message = 'Title is too short';
+  } else if (title.length < 15) {
+    message = 'Title is extra';
+  } else { 
+    message = 'Title is too long';
   }
+  
 
   return (
     <div>
       <h1>My favourite movies to watch</h1>
       <h2>My favourite movie for today is {title} </h2>
+      {
+        title.length > 0 && <div>{message}</div>
+      }
       <input type="text" onChange={handleChange}/>
     </div>
   
