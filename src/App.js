ba5b5d7b534aefa777529ce53cead9aa93a11604
Pawel Wludyka <pwludyka@op.pl>
2026-01-17 11:43:06 +0100

    movieadd1

--- src/App.js
+++ src/App.js
@@ -1,49 +1,17 @@
 import './App.css';
 import {useState} from "react";
 import "milligram";
+import MovieForm from "./MovieForm";
+import MoviesList from "./MoviesList";
 
 function App() {
-    const [title, setTitle] = useState('');
-    const [year, setYear] = useState('');
     const [movies, setMovies] = useState([]);
-
-    let message;
-    if (title.length < 5) {
-        message = <div>Tutuł jest za krótki. Nagrywają takie filmy?</div>;
-    } else if (title.length < 15) {
-        message = <div>Tytuł jest ekstra, w sam raz na plakat przed kinem!</div>;
-    } else {
-        message = <div>Tytuł jest za długi, nikt tego nie zapamięta.</div>;
-    }
-
-    function handleChange(event) {
-        setTitle(event.target.value);
-    }
-    function handleChangeYear(event) {
-        setYear(event.target.value);
-    }
-    function handleAddMovie(event) {
-        const movie = {title: title, year: year};
-        setMovies([...movies, movie]);
-        setTitle('')
-        setYear('')
-    }
 
     return (
         <div>
-            <h1>Add Movie</h1>
-            <h2>Titles</h2>
-            <ul>
-                {movies.map(movie => <li key={movie.title}>{movie.title} ({movie.year})</li>)}
-            </ul>
-            <h2>Add movie</h2>
-            <label>Tytuł</label>
-            <input type="text" value={title} onChange={handleChange}/>
-            {title.length > 0 && <div>{message}</div>}
-
-            <label>Rok nagrania</label>
-            <input type="text" value={year} onChange={handleChangeYear}/>
-            <button onClick={handleAddMovie}>Dodaj Film</button>
+            <h1>My favourite movies to watch</h1>
+            <MoviesList movies={movies}/>
+            <MovieForm onMovieSubmit={(movie) => setMovies([...movies, movie])}/>
         </div>
     );
 }
